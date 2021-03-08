71d7c9a91445df9c471f9f76c68d68ceb951c0a2
Ayadi Tahar <ayaditahero2016@gmail.com>
2021-03-09 00:50:04 +0100

    Format Store
    Improved Store style by adding combineReducers

--- src/store/fileHistory.tsx
+++ src/store/fileHistory.tsx
@@ -1,28 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const slice = createSlice({
-  name: 'fileHistory',
+  name: `fileHistory`,
   initialState: [],
   reducers: {
-    filesLoaded: (state, action) => {
+    filesLoaded: () => {
       if (localStorage.getItem(`file-history`)) {
         const f = `[${localStorage.getItem(`file-history`)}]`;
         return JSON.parse(f);
       }
       return [];
     },
-    fileAdded: (state, action) => {
-      return [
-        ...state,
-        {
-          id: action.payload.id,
-          name: action.payload.name,
-          lastModified: action.payload.lastModified,
-          size: action.payload.size,
-          type: action.payload.type,
-        },
-      ];
-    },
+    fileAdded: (state, action) => [
+      ...state,
+      {
+        id: action.payload.id,
+        name: action.payload.name,
+        lastModified: action.payload.lastModified,
+        size: action.payload.size,
+        type: action.payload.type,
+      },
+    ],
     fileRemoved: (state, action) =>
       state.filter((file) => file.id !== action.payload.id),
   },
@@ -30,3 +28,5 @@ const slice = createSlice({
 
 export const { filesLoaded, fileAdded, fileRemoved } = slice.actions;
 export default slice.reducer;
+
+// Selectors
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import reducer from './fileHistory';
+import reducer from './reducer';
 
 export default configureStore({
   reducer,
